ed666a66b25d04884c6078b135db5bc64d246dba
kashiken <gebizo3@gmail.com>
2013-08-13 12:47:15 +0900

    first

---
+++ cookbooks/httpd-php/recipes/default.rb
@@ -0,0 +1,26 @@
+%w{ httpd php php-pecl-memcache }.each do |pkg|
+  package pkg do
+    action :install
+  end
+end
+
+service "httpd" do
+  supports :status => true, :restart => true, :reload => true
+  action [ :enable, :start ]
+end
+
+template "/var/www/html/info.php" do
+  source "info.php.erb"
+  owner "apache"
+  group "apache"
+  mode 0644
+end
+
+template "/etc/httpd/conf/httpd.conf" do
+  source "httpd.conf.erb"
+  owner "root"
+  group "root"
+  mode 0644
+  notifies :reload, 'service[httpd]'
+end
+
